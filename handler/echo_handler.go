1854f80d6d716856ee252530adb11984376dee7e
sala <saladinkzn@gmail.com>
2019-10-22 23:16:57 +0300

    Handle empty answer

--- handler/echo_handler.go
+++ handler/echo_handler.go
@@ -13,20 +13,33 @@ func CreateEchoHandler() *EchoHandler {
 }
 
 func (this EchoHandler) Handle(question *alice.Question) (answer *alice.Answer, err error) {
+	originalUtterance := question.Request.OriginalUtterance
+	var text string
+	if len(originalUtterance) == 0 {
+		text = "Пожалуйста, введите текст"
+	} else {
+		text = question.Request.OriginalUtterance
+	}
+
+	answer = makeAnswer(text, question.Version, question.Session)
+	return
+}
+
+func makeAnswer(text string, version string, session alice.Session) *alice.Answer {
 	response := alice.Response {
-		Text: question.Request.OriginalUtterance,
-		TTS: question.Request.OriginalUtterance,
+		Text: text,
+		TTS: text,
 		EndSession: false,
 		Buttons: make([]alice.Button, 0),
 	}
 
-	answer = &alice.Answer {
-		Version: question.Version,
-		Session: convertSession(question.Session),
+	answer := &alice.Answer {
+		Version: version,
+		Session: convertSession(session),
 		Response: response,
 	}
 
-	return
+	return answer
 }
 
 func convertSession(session alice.Session) alice.Session {
